1269cadd86b39c27a11d703a96b4d7af3facd032
Bhaumik Soni <bhaumik.soni@agileinfoways.com>
2018-03-28 11:48:51 +0530

    initial commit

---
+++ app/src/main/java/com/production/checkproductiontype/MainActivity.java
@@ -0,0 +1,22 @@
+package com.production.checkproductiontype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.tv_type);
+
+        if (BuildConfig.DEBUG) {
+            // do something for a debug build
+            textView.setText("QA");
+        } else textView.setText("Production");
+    }
+}
